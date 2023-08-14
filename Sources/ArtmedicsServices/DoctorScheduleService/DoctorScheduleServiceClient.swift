5987ff0d92cbf2a2d1f7928b50988cef35c7b797
Nikolai Faustov <faustov.nikolay@gmail.com>
2023-08-14 20:26:29 +0300

    Fixes

--- Sources/ArtmedicsServices/DoctorScheduleService/DoctorScheduleServiceClient.swift
+++ Sources/ArtmedicsServices/DoctorScheduleService/DoctorScheduleServiceClient.swift
@@ -39,9 +39,7 @@ private extension Endpoint {
     }
 
     static func getByDate(_ date: Date) -> Self {
-        let formatter = DateFormatter()
-        formatter.timeZone = TimeZone(secondsFromGMT: 10_800)
-        let query = URLQueryItem(name: "date", value: "\(formatter.string(from: date))")
+        let query = URLQueryItem(name: "date", value: "\(date.timeIntervalSince1970)")
         
         return Endpoint(path: "/schedules", queryItems: [query])
     }
